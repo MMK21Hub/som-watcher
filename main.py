88b7cc5645de5226b1e7bd40efbad3189ab8556b
MMK21Hub <50421330+MMK21Hub@users.noreply.github.com>
2025-08-23 01:48:47 +0100

    Implement the thing

--- main.py
+++ main.py
@@ -1,13 +1,59 @@
+from __future__ import annotations
+from math import inf
 from sys import stderr
 from time import sleep
 from traceback import format_exc
 from argparse import ArgumentParser
 from prometheus_client import start_http_server, Gauge
+from typing import List, Optional
+from pydantic import BaseModel, Field, ValidationError
+import requests
+
+# class Prices(BaseModel):
+#     us: Optional[int] = Field(None, alias="US")
+#     eu: Optional[int] = Field(None, alias="EU")
+#     india: Optional[int] = Field(None, alias="IN")
+#     ca: Optional[int] = Field(None, alias="CA")
+#     au: Optional[int] = Field(None, alias="AU")
+#     worldwide: Optional[int] = Field(None, alias="XX")
+
+SHOP_API_URL = "https://summer.skyfall.dev/api/shop"
+REGION_NAMES = {
+    "US": "United States",
+    "EU": "EU + UK",
+    "IN": "India",
+    "CA": "Canada",
+    "AU": "Australia",
+    "XX": "Rest of World",
+}
 
 
-def fetch_data():
-    # TODO - Implement data fetching
-    pass
+class ShopItem(BaseModel):
+    title: str
+    image_url: str = Field(..., alias="imageUrl")
+    description: str
+    purchase_url: str = Field(..., alias="purchaseUrl")
+    id: int
+    is_black_market: bool = Field(..., alias="isBlackMarket")
+    prices: dict[str, int]
+    stock_remaining: Optional[int] = Field(None, alias="stockRemaining")
+
+
+def fetch_shop() -> list[ShopItem]:
+    response = requests.get(SHOP_API_URL)
+    response.raise_for_status()
+    data = response.json()
+    if not data:
+        raise ValueError("No data received from shop API")
+    items: list[ShopItem] = []
+    for item in data:
+        try:
+            items.append(ShopItem(**item))
+        except ValidationError as e:
+            print(f"Failed to parse item: {e}", file=stderr)
+    if not items:
+        raise ValueError("Failed to parse shop items from the API")
+    return items
 
 
 def main():
@@ -15,7 +61,7 @@ def main():
     parser.add_argument(
         "--port",
         type=int,
-        default=9000,
+        default=9040,
         help="the port to run the Prometheus exporter on",
     )
     parser.add_argument(
@@ -27,7 +73,7 @@ def main():
     parser.add_argument(
         "--interval",
         type=int,
-        default=5,
+        default=10,
         help="how often to fetch data, in seconds",
     )
     args = parser.parse_args()
@@ -36,14 +82,50 @@ def main():
     print(f"Started metrics exporter: http://localhost:{args.port}/metrics", flush=True)
 
     has_had_success = False
-    # TODO - Set up metrics
+    stock_gauge = Gauge(
+        "som_shop_stock",
+        "Stock remaining for shop items",
+        ["item_id", "item_name", "item_image", "shop_type", "item_description"],
+    )
+    price_gauge = Gauge(
+        "som_shop_price_shells",
+        "Price for shop items",
+        [
+            "item_id",
+            "item_name",
+            "item_image",
+            "shop_type",
+            "item_description",
+            "region",
+            "region_name",
+        ],
+    )
 
     while True:
         try:
-            leaderboard = fetch_data()
-            # TODO - Update metrics
+            items = fetch_shop()
+            for item in items:
+                shop_type = "black_market" if item.is_black_market else "public_shop"
+                stock_gauge.labels(
+                    item_id=item.id,
+                    item_name=item.title,
+                    item_image=item.image_url,
+                    shop_type=shop_type,
+                    item_description=item.description,
+                ).set(item.stock_remaining or +inf)
+                for region, price in item.prices.items():
+                    region_name = REGION_NAMES.get(region) or region
+                    price_gauge.labels(
+                        item_id=item.id,
+                        item_name=item.title,
+                        item_image=item.image_url,
+                        shop_type=shop_type,
+                        item_description=item.description,
+                        region=region,
+                        region_name=region_name,
+                    ).set(price)
             if args.verbose:
-                print(f"Successfully fetched data")
+                print(f"Successfully fetched {len(items)} shop items")
             has_had_success = True
         except Exception as e:
             # Exit the program if the first fetch fails
